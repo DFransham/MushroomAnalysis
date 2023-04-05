b8803d12b0cca15b7b6e079df376e0efc5247cc7
DFransham <115869560+DFransham@users.noreply.github.com>
2023-04-06 11:35:19 +1200

    got the program working to 0.98 kappa score

--- mushroom.py
+++ mushroom.py
@@ -1,22 +1,30 @@
+#declare varialbes for use later in program
 mushIn = []
 mushOut = []
-
 mushFeatures = [7,21,22,27,37,52,63,67,97,104,110]
+mushAttributes = ['feature_7','feature_21','feature_22','feature_27','feature_37','feature_52','feature_63','feature_67','feature_97','feature_104','feature_110']
+mushEdible = ['false','true']
+outputValues = []
+iOutputValue = 0
 
+#open file
 mushFile = open ('mushroom.txt', 'r')
 
+#read line from file
 mushFileLine = mushFile.readline()
 
+#loop through file to collect values - continues while line is not null
 while mushFileLine:
-    mushInLine = []
-    if len(mushFileLine) > 1:
-        mushFileLine = mushFileLine.strip()
-        mushFileLine = mushFileLine.split(',')
-        for i in mushFeatures:
+    mushInLine = [] #temporary storage for values to add to mushIn later
+    
+    if len(mushFileLine) > 1:   #only operate on line if it has length
+        mushFileLine = mushFileLine.strip()     #strip /n from line
+        mushFileLine = mushFileLine.split(',')  #split by comma into individual variables
+        for i in mushFeatures:                  #only collect values corresponding to what we want
             mushInLine.append(int(mushFileLine[i]))
         #print(mushInLine)
         mushIn.append(mushInLine)
-        mushOut.append(int(mushFileLine[121]))
+        mushOut.append(int(mushFileLine[122]))
     mushFileLine = mushFile.readline()
 mushFile.close()
 #print(mushOut)
@@ -25,12 +33,6 @@ mushFile.close()
 import clips
 env = clips.Environment()
 env.load('mushroom.clp')
-
-mushAttributes = ['feature_7','feature_21','feature_22','feature_27','feature_37','feature_52','feature_63','feature_67','feature_97','feature_104','feature_110']
-mushEdible = ['false','true']
-
-outputValues = []
-iOutputValue = 0
 
 for line in mushIn:
     #assert the values as CLIPS facts
@@ -55,14 +57,16 @@ for line in mushIn:
     outputValues.append(iOutputValue)
     env.reset()
 
-#print(outputValues)
-#print(mushOut)
+print(outputValues)
+print(mushOut)
 
+#get accuracy score (percentage)
 from sklearn.metrics import accuracy_score
 PctAccuracy = accuracy_score (mushOut, outputValues)
 PctAccuracy = PctAccuracy * 100
 print ('Percent: %.2f' % PctAccuracy)
 
+#get accuracy score (cohen-kappa)
 from sklearn.metrics import cohen_kappa_score
 Kappa = cohen_kappa_score (mushOut, outputValues)
 print ('Kappa: %.2f' % Kappa)
